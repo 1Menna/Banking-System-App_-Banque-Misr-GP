539cf5a67a24ace6ee9b9a3a0c82134d76225427
1Menna <meno903010@gmail.com>
2025-08-27 16:04:22 +0300

    user-home && admin-home && remain small parts to finish

--- src/app/components/admin-home/admin-home.ts
+++ src/app/components/admin-home/admin-home.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-admin-home',
-  imports: [],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule, RouterLink],
   templateUrl: './admin-home.html',
   styleUrl: './admin-home.css'
 })
--- src/app/components/user-home/user-home.ts
+++ src/app/components/user-home/user-home.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {ChangeDetectionStrategy} from '@angular/core';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user-home',
-  imports: [],
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule, RouterLink],
   templateUrl: './user-home.html',
   styleUrl: './user-home.css'
 })
 export class UserHome {
-
+  longText = `The Chihuahua is a Mexican breed of toy dog. It is named for the
+  Mexican state of Chihuahua and is among the smallest of all dog breeds. It is
+  usually kept as a companion animal or for showing.`;
 }
