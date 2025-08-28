5cea079d14c981ad1338af08aa768b016aa8e1f4
hagergoda2 <hgoda2258@gmail.com>
2025-08-28 04:35:24 +0300

    guard

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -3,38 +3,59 @@ import { CanActivateFn, Router } from '@angular/router';
 import { Auth } from '../services/auth';
 import { isPlatformBrowser } from '@angular/common';
 
-export const authGuard: CanActivateFn = (route, state) => {
-
+  export const authGuard: CanActivateFn = (route, state) => {
   const _authService = inject(Auth);
   const _Router = inject(Router);
-  const _PLATFORM_ID=inject(PLATFORM_ID)
-  const currentUser = _authService.getCurrentUser();
+  const _PLATFORM_ID = inject(PLATFORM_ID);
 
-  // if (!_authService.isLoggedIn()) {
-  //   _Router.navigate(['/']);
-  //   return false;
-  // }
-  // const expectedRole = route.data['role'];
-  // if (expectedRole && currentUser?.role !== expectedRole) {
-   
-  //   _Router.navigate(['/']);
-  //   return false;
-  // }
-  // return true;
-  if(isPlatformBrowser(_PLATFORM_ID)){
-     if (!_authService.isLoggedIn()) {
-    _Router.navigate(['/login']);
-    return false;
+  if (isPlatformBrowser(_PLATFORM_ID)) {
+    if (!_authService.isLoggedIn()) {
+      return _Router.createUrlTree(['/login']);
+    }
+
+    const currentUser = _authService.getCurrentUser();
+    const expectedRole = route.data['role'];
+
+    if (expectedRole && currentUser?.role !== expectedRole) {
+      return _Router.createUrlTree(['/']);
+    }
+
+    return true;
   }
-  const expectedRole = route.data['role'];
-  if (expectedRole && currentUser?.role !== expectedRole) {
-   
-    _Router.navigate(['/login']);
-    return false;
-  }
+
+  
   return true;
-  }
-  else{
-    return false;
-  } 
 };
+
+
+//   const _authService = inject(Auth);
+//   const _Router = inject(Router);
+//   const _PLATFORM_ID=inject(PLATFORM_ID)
+//   const currentUser = _authService.getCurrentUser();
+
+//   // if (!_authService.isLoggedIn()) {
+//   //   _Router.navigate(['/']);
+//   //   return false;
+//   // }
+//   // const expectedRole = route.data['role'];
+//   // if (expectedRole && currentUser?.role !== expectedRole) {
+   
+//   //   _Router.navigate(['/']);
+//   //   return false;
+//   // }
+//   // return true;
+//   if(isPlatformBrowser(_PLATFORM_ID)){
+//      if (!_authService.isLoggedIn()) {
+//       return _Router.createUrlTree(['/login']);
+//   }
+//   const expectedRole = route.data['role'];
+//   if (expectedRole && currentUser?.role !== expectedRole) {
+
+//     return _Router.createUrlTree(['/']);
+//   }
+//   return true;
+//   }
+//   else{
+//     return false;
+//   } 
+// };
