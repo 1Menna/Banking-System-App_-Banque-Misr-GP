52861cd1ae071d6c17b4aee32a1e4798c38c4d17
1Menna <meno903010@gmail.com>
2025-08-28 15:53:36 +0300

    my-account

--- src/app/components/my-account/my-account.ts
+++ src/app/components/my-account/my-account.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
-
+import { CurrencyPipe } from '@angular/common';
 @Component({
   selector: 'app-my-account',
-  imports: [],
+   imports: [CurrencyPipe],
   templateUrl: './my-account.html',
-  styleUrl: './my-account.css'
+  styleUrls: ['./my-account.css']
 })
 export class MyAccount {
+  account = {
+    name: "John Doe",
+    accountNumber: "1234 5678 9101",
+    balance: 2500.75,
+    email: "john.doe@email.com",
+    phone: "+20123456789"
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
