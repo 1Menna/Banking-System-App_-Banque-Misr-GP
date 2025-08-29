77f229abf7946802975aa0c9f8e948a40da44663
Monica <monicamaged5@gmail.com>
2025-08-29 23:17:24 +0300

    Add HttpClient

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
-
+import { importProvidersFrom } from '@angular/core';
 import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,Footer],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
   protected readonly title = signal('banking_system_app');
